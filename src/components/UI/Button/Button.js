f2ad0b1bca65cddd4d6493df22f3717d68dbfbb3
Amol shelke <shelkeamol897@gmail.com>
2022-11-14 17:06:12 +0530

    Added Styled component and Media queries

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -18,6 +19,10 @@ const Button = styled.button`
     background: #ac0e77;
     border-color: #ac0e77;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+
+  @media (min-width: 700px) {
+    width: auto;
   }
 `;
 
